ebd6a699122304585a3845283bb523aceb33c798
mattrichor <matt.a.geyer@gmail.com>
2022-10-04 13:08:45 -0400

    test to fix cors issue

--- server.js
+++ server.js
@@ -14,14 +14,21 @@ app.use(
     extended: true
   })
 )
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
+//Code below from Symentix.com to help debug 500 errors
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  console.error(err)
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: err
+  })
 })
+
 app.use(express.json())
 
 app.use('/api', routes)
